20843cf085896ee62a2b0ae7436f5ff2dcbbaf7f
Andrew Wang <aywang71@gmail.com>
2021-06-30 19:09:39 -0400

    knn on demographic subsets

--- main.R
+++ main.R
@@ -5,13 +5,18 @@ rm(list=ls()) # clean up any old stuff in R
 setwd("~/GitHub/2020-demographic-indicator-ml") # go to this folder
 
 #load up myfunctions.R
-source("~/GitHub/2020-demographic-indicator-ml")
+source("~/GitHub/2020-demographic-indicator-ml/myfunctions.R")
 
 #library import
 library(maps)
 library(tidyverse)
 library(usmap)
 library(gridExtra)
+library(caret)
+library(class)
+library(ggvis)
+library(gmodels)
+library(GGally)
 
 set.seed(1234)
 
@@ -24,19 +29,47 @@ print(sum(is.na(data)))
 data$outcome <- ifelse(data$percentage16_Donald_Trump>data$percentage16_Hillary_Clinton,-1,1) # -1 = republican, 1 = democrat
 
 ind <- sample(2, nrow(data), replace = TRUE, prob = c(0.7, 0.3))
-populationTraining <- data[ind == 1, c(16:20,27)]
-populationTesting <- data[ind == 2, c(16:20,27)]
-raceTraining <- data[ind == 1, 21:26]
-raceTesting <- data[ind == 2, 21:26]
-transportationTraining <- data[ind == 1, c(38:43,45)]
-transportationTesting <- data[ind == 2, c(38:43,45)]
-incomeTraining <- data[ind == 1, c(28,30,31:32)]
-incomeTesting <- data[ind == 2, c(28,30,31:32)]
-employmentTraining <- data[ind == 1, c(33:37,44)]
-employmentTesting <- data[ind == 1, c(33:37,44)]
-sectorTraining <- data[ind == 1, 47:50]
-sectorTesting <- data[ind == 2, 47:50]
 trainingLabels <- data[ind == 1, 52]
 testingLabels <- data[ind == 2, 52]
+
+populationTraining <- data[ind == 1, c(16:20,27)]
+populationTesting <- data[ind == 2, c(16:20,27)]
+set.seed(1234)
+populationPrediction <- knn(train = populationTraining, test = populationTesting, cl = trainingLabels, k = 3)
+CrossTable(x = testingLabels, y = populationPrediction, prop.chisq = FALSE)
+
+raceTraining <- data[ind == 1, 21:26]
+raceTesting <- data[ind == 2, 21:26]
+set.seed(1234)
+racePrediction <- knn(train = raceTraining, test = raceTesting, cl = trainingLabels, k = 3)
+CrossTable(x = testingLabels, y = racePrediction, prop.chisq = FALSE)
+
+transportationTraining <- data[ind == 1, c(38:43,45)]
+transportationTesting <- data[ind == 2, c(38:43,45)]
+set.seed(1234)
+transportationPrediction <- knn(train = transportationTraining, test = transportationTesting, cl = trainingLabels, k = 3)
+CrossTable(x = testingLabels, y = transportationPrediction, prop.chisq = FALSE)
+
+incomeTraining <- data[ind == 1, c(28,30,31:32)]
+incomeTesting <- data[ind == 2, c(28,30,31:32)]
+set.seed(1234)
+incomePrediction <- knn(train = incomeTraining, test = incomeTesting, cl = trainingLabels, k = 3)
+CrossTable(x = testingLabels, y = incomePrediction, prop.chisq = FALSE)
+
+employmentTraining <- data[ind == 1, c(33:37,44)]
+employmentTesting <- data[ind == 1, c(33:37,44)]
+set.seed(1234)
+employmentPrediction <- knn(train = employmentTraining, test = employmentTesting, cl = trainingLabels, k = 3)
+CrossTable(x = testingLabels, y = employmentPrediction, prop.chisq = FALSE)
+
+sectorTraining <- data[ind == 1, 47:50]
+sectorTesting <- data[ind == 2, 47:50]
+set.seed(1234)
+sectorPrediction <- knn(train = sectorTraining, test = sectorTraining, cl = trainingLabels, k = 3)
+CrossTable(x = testingLabels, y = sectorPrediction, prop.chisq = FALSE)
+
+
+
+
 
 #cluster size is 3
